b42eafc7f9ef53c2c6eaeb9a606d081a1552fabc
JackWang <jack4shihong@gmail.com>
2022-06-06 23:48:03 +0800

    finished commit and sync

--- sync.sh
+++ sync.sh
@@ -1,10 +1,11 @@
 #! /bin/bash
 
 # backup
+time=$(date "+%Y%m%d.%H%M%S")
 echo "User home directory: ${HOME}"
 echo "Backup previous config"
 lvim_path=${HOME}/.config/lvim
-mv ${lvim_path}/config.lua ${lvim_path}/config.lua.backup
+mv ${lvim_path}/config.lua ${lvim_path}/config.lua.backup."${time}"
 
 # update
 echo "Updating config"
@@ -13,3 +14,4 @@ cp ${shell_folder}/config.lua ${lvim_path}/config.lua
 
 
 echo "Successfully sync config.lua at ${lvim_path}/config.lua"
+echo "Backup can be found in ${lvim_path}/config.lua.backup.${time}"
