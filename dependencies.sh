6aeced2f4b914bbb810fefeba771cf2c7e69c811
JackWang <jack3shihong@gmail.com>
2022-07-01 12:17:04 +0800

    fixed color bug and add neovim installation

--- dependencies.sh
+++ dependencies.sh
@@ -7,7 +7,7 @@ normal="\033[0m"
 echo -e "${green}Updating package index...${normal}"
 sudo apt-get update
 
-echo -e "${green}Installing npm...{normal}"
+echo -e "${green}Installing npm...${normal}"
 sudo apt-get install npm
 
 echo -e "${green}Fix npm eacces permission errors...${normal}"
@@ -19,26 +19,29 @@ source ~/.profile
 echo -e "${green}Npm eacces permission testing... (this may take a while)${normal}"
 npm install -g jshint
 
-echo -e "${green}Installing curl...{normal}"
+echo -e "${green}Installing curl...${normal}"
 sudo apt-get install curl
 
-echo -e "${green}Installing unzip...{normal}"
+echo -e "${green}Installing unzip...${normal}"
 sudo apt-get install unzip
 
 
 # install fonts
-echo -e "${green}Installing nerd agave fonts... (this may take a while){normal}"
+echo -e "${green}Installing nerd agave fonts... (this may take a while)${normal}, you'd better download it via web brwser and install manually"
 wget -c https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Agave.zip
 mkdir ~/.fonts/
 unzip Agave.zip -d ~/.fonts
 fc-cache -fv
 
-
 # install rust
 echo -e "${green}Installing rust cargo...${normal}"
-echo -e "${green}Downloading... (this may take a while)${normal}"
+echo -e "${green}Downloading... (this may take a long time)${normal}"
 curl https://sh.rustup.rs -sSf | sh
 
+# install neovim
+sudo add-apt-repository ppa:neovim-ppa/unstable
+sudo apt-get update
+sudo apt-get install neovim
 
 
 echo -e "${green}Successfully download all dependencies, run install.sh to install lunarvim${normal}"
